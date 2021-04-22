8dfa5c44fa8bae59dffd33c05f06af6af14bcae3
Jason Yee <jasonyee1776@gmail.com>
2021-04-21 22:55:23 -0700

    update style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+// let projectAnimation = anime({
+//     targets: 'li',
+//     translateX: 250,
+//     borderRadius: 50,
+//     rotate: '1turn',
+//     backgroundColor: '#FFF',
+//     duration: 800
+//   });   
